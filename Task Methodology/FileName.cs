3375c877912c9968e00567033f285767bedde825
byaviator <abdullahmustafayev@icloud.com>
2025-03-14 03:58:42 +0400

    Task oldu

---
+++ Task Methodology/FileName.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Methodology
+{
+    class FileName
+    {
+        public static string ArrayToString(int[] array)
+        {
+            return string.Join(" ", array);
+        }
+
+
+        public static int Sum(int[] array)
+        {
+            int sum = 0 ;
+            foreach (var number in array)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+
+        public static bool ContainsNegative(int[] array)
+        {
+            foreach (var number in array)
+            {
+                if (number < 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static int Multiply(params int[] numbers)
+        {
+            int result = 1;
+            foreach (var number in numbers)
+            {
+                result *= number;
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Task Methodology/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task_Methodology
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, -4, 9, -8 };
+
+            string arrayString = FileName.ArrayToString(array);
+            int sum = FileName.Sum(array);
+            bool hasNegative = FileName.ContainsNegative(array);
+
+            Console.WriteLine(arrayString);
+            Console.WriteLine(sum);
+            Console.WriteLine(hasNegative);
+            
+
+        }
+    }
+}
